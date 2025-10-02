ce7f8e0c77c7238743632e5ae8e78dd3c8cec735
Sab <skiirford@gmail.com>
2025-10-02 10:57:44 -0300

    feat(model): add enum for timer states

--- src/main/java/com/voyydless/pomodoro/Main.java
+++ src/main/java/com/voyydless/pomodoro/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.voyydless.pomodoro;
 
 public class Main {
     public static void main(String[] args) {
---
+++ src/main/java/com/voyydless/pomodoro/model/TimerState.java
@@ -0,0 +1,8 @@
+package com.voyydless.pomodoro.model;
+
+public enum TimerState {
+    WORK,
+    SHORT_BREAK,
+    LONG_BREAK,
+    STOPPED
+}
